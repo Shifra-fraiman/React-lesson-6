b0aef07b3686e416615ad60a7195dde9ee5e5a41
shifra-fraiman <s0548549680@gmail.com>
2023-11-28 23:50:16 +0200

    more use at UserContext

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useContext } from 'react'
 import { customers } from "../assets/Data/customers.js";
+import { UserContext } from '../context/user.context.jsx';
 
 export const Statistics=()=>{
+    const user = useContext(UserContext);
     const nunOfCustomers=customers.length;
     const customersFromThisYear=(customers.filter(customer=> customer.dateJoin==="2023")).length;
     return <div>
-        In this CRM has {nunOfCustomers} customers and {customersFromThisYear} from this year 
+        {user.name}, want to join?
+        In this CRM has {nunOfCustomers} customers and {customersFromThisYear} from this year!
     </div>
 }
